bbc4ed6c4d3c7865839e31c78ce0459d5c31ce73
lloviazuu <sungjoo3321@gmail.com>
2025-07-31 00:18:45 +0900

    Update src/styles/global.js
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,6 +4,12 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset}
   body {
+    @supports (height: 100svh) { height: 100svh; }
+    @supports not (height: 100svh) { height: 100vh; }
+    @media (max-width: 375px) {
+      @supports (width: 100svw) { width: 100svw; }
+      @supports not (width: 100svw) { width: 100vw; }
+    }
     font-family: 'Noto Sans KR', sans-serif;
     background-color: #fff;
     margin: 0;
