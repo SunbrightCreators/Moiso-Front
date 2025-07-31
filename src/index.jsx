cbdc5c7b04ad5189d4bb6b59ce3f45fb261b9ea2
lloviazuu <sungjoo3321@gmail.com>
2025-07-31 23:11:47 +0900

    :wrench: index.jsx수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { register } from "./serviceWorkerRegistration";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+import GlobalStyle from './styles/global';
+import Router from './Router'; // App → Router로 변경
+import './styles/reset.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
-register();
