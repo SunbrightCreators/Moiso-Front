9905e3e212bc3c7d88caa9c1b0c89c44882df143
lloviazuu <sungjoo3321@gmail.com>
2025-07-31 23:48:29 +0900

    :wrench:reset.css

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,19 +1,98 @@
 import { createGlobalStyle } from 'styled-components';
 
-
 const GlobalStyle = createGlobalStyle`
-  ${reset}
-  body {
-    @supports (height: 100svh) { height: 100svh; }
-    @supports not (height: 100svh) { height: 100vh; }
-    @media (max-width: 375px) {
-      @supports (width: 100svw) { width: 100svw; }
-      @supports not (width: 100svw) { width: 100vw; }
-    }
-    font-family: 'Noto Sans KR', sans-serif;
-    background-color: #fff;
+  *, *::before, *::after {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
+    border: 0;
+    vertical-align: baseline;
+    text-decoration: none;
+    outline: none;
+    background-color: transparent;
+  }
+
+  body {
+    line-height: 1;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', sans-serif;
+    background-color: #fff;
+    color: #000;
+  }
+
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+
+  a,
+  a:link,
+  a:visited,
+  a:active {
+    text-decoration: none;
+    color: unset;
+  }
+
+  ol,
+  ul,
+  li {
+    list-style: none;
+  }
+
+  blockquote,
+  q {
+    quotes: none;
+  }
+
+  blockquote::before,
+  blockquote::after,
+  q::before,
+  q::after {
+    content: '';
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  button {
+    background: none;
+    cursor: pointer;
+    border: 0;
+    padding: 0;
+  }
+
+  input[type='button'],
+  input[type='submit'],
+  input[type='reset'],
+  input[type='radio'],
+  label {
+    cursor: pointer;
+  }
+
+  input[type='radio'] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+  }
+
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
   }
 `;
 
