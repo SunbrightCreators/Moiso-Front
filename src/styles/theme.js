4c40bc1409ed1c45317109a3b48cc5e4ebfeee0f
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-01 19:56:43 +0900

    ♻️ theme 개선 (#1)

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,6 +1,10 @@
-export const theme = {
-  colors: {
-    primary: '#3498db',
-    gray: '#bdc3c7',
-  },
+const colors = {
+  primary: '#3498db',
+  gray: '#bdc3c7',
 };
+
+const theme = {
+  colors,
+};
+
+export { theme };
