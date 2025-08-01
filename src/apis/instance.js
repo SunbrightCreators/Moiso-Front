49a0ec2ef29c2f0c3c32a41b568514d94f3ca93d
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-01 17:43:01 +0900

    🚚 디렉토리 구조 수정 (#1)

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  timeout: 5000,
+});
+
+export default axiosInstance;
+
+
+import axios from 'axios';
+
 const token = JSON.parse(localStorage.getItem('token'));  // 예: { grantType: 'Bearer', accessToken: 'abc...' }
 
 const authHeader = token ? `${token.grantType} ${token.accessToken}` : 'null';
