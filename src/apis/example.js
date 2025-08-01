b38d88d1f58988609ac5546522b543e532c15edf
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-01 19:23:09 +0900

    ♻️ API호출함수예시 반환로직 개선 (#1)

--- src/apis/example.js
+++ src/apis/example.js
@@ -4,52 +4,47 @@ import { authClient } from './instance';
  * GET 요청
  */
 const getExample = async () => {
-  const response = await authClient.get(`/example`);
-  return response;
+  return await authClient.get(`/example`);
 };
 
 /**
  * POST 요청
  */
 const postExample = async (title, content) => {
-  const response = await authClient.post(
+  return await authClient.post(
     `/example`,
     { title, content },
     { headers: { 'Content-Type': 'application/json' } },
   );
-  return response;
 };
 
 /**
  * PUT 요청
  */
 const putExample = async (title, content) => {
-  const response = await authClient.put(
+  return await authClient.put(
     `/example`,
     { title, content },
     { headers: { 'Content-Type': 'application/json' } },
   );
-  return response;
 };
 
 /**
  * PATCH 요청
  */
 const patchExample = async (content) => {
-  const response = await authClient.patch(
+  return await authClient.patch(
     `/example`,
     { content },
     { headers: { 'Content-Type': 'application/json' } },
   );
-  return response;
 };
 
 /**
  * DELETE 요청
  */
 const deleteExample = async () => {
-  const response = await authClient.delete(`/example`);
-  return response;
+  return await authClient.delete(`/example`);
 };
 
 export { getExample, postExample, putExample, patchExample, deleteExample };
