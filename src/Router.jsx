fe8602389e06a824b7bd92332b41e5d1cb593688
lloviazuu <sungjoo3321@gmail.com>
2025-08-03 17:15:59 +0900

    :lipstick:스피너 공통 컴포넌트 퍼블리싱(#5)

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import SpinnerComponent from './components/common/SpinnerComponent';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h1>홈</h1>
+              <h2>스피너 테스트</h2>
+              <SpinnerComponent />
+            </div>
+          }
+        />{' '}
         <Route path='/' element={<div>홈</div>} />
       </Routes>
     </BrowserRouter>
