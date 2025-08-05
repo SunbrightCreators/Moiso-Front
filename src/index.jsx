61df3f913ab8b1a75ce58d8fe514db5e9b85032a
lloviazuu <sungjoo3321@gmail.com>
2025-08-06 01:03:53 +0900

    :lipstick: 공통 컴포넌트
    src/index.jsx 수정

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import './styles/reset.css';
+import 'antd/dist/reset.css';
 import { theme } from './styles/theme';
 import GlobalStyle from './styles/global';
 import Router from './Router';
