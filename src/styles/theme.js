d4c5db4834c9eeacd7ae20408001b594a8a91dd9
lloviazuu <sungjoo3321@gmail.com>
2025-08-09 19:56:40 +0900

    Update src/styles/theme.js
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -20,20 +20,20 @@ const tokens = defineTokens({
 });
 
 // 2) 글로벌 스타일 정의
-const GlobalStyle = defineGlobalStyles({
-  body: {
-    '@supports(height: 100svh)': { height: '100svh' },
-    '@supports not (height: 100svh)': { height: '100vh' },
-    '@media(max-width:360px)': {
-      '@supports(width:100svw)': { width: '100svw' },
-      '@supports not(width:100svw)': { width: '100vw' },
-    },
-    '@media(min-width:360px)': {
-      width: '360px',
-      margin: '0 auto',
-    },
+const globalCss = {
+  'body': {
+    @supports (height: 100svh) { height: 100svh; }
+    @supports not (height: 100svh) { height: 100vh; }
+    @media (max-width: 360px) {
+      @supports (width: 100svw) { width: 100svw; }
+      @supports not (width: 100svw) { width: 100vw; }
+    }
+    @media (min-width: 360px) {
+      width: 360px;
+      margin: 0 auto;
+    }
   },
-});
+};
 export const theme = {
   colors: {
     primary: tokens.colors.primary.value,
