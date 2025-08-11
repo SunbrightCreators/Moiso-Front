784e0812117abfb392cdf1a8d0fcd98a5bea3aaa
Minhye Kim <kimilu@ewha.ac.kr>
2025-08-11 15:43:54 +0900

    💄Dialog 공통 컴포넌트 코드 수정(#7)

--- src/components/common/DialogComponent.jsx
+++ src/components/common/DialogComponent.jsx
@@ -1,27 +1,31 @@
-import { Button, Dialog, Portal, CloseButton } from "@chakra-ui/react";
+import { Dialog, DialogContent,DialogHeader,DialogBody,DialogFooter,DialogBackdrop,DialogCloseTrigger,Button,DialogTitle
+} from "@chakra-ui/react";
 
-const DialogComponent = ({ isOpen, onClose, title, content, actionText = "확인", onAction }) => (
-  <Dialog.Root open={isOpen} onOpenChange={(e) => !e.open && onClose()}>
-    <Portal>
-      <Dialog.Backdrop />
-      <Dialog.Positioner>
-        <Dialog.Content>
-          <Dialog.Header>
-            <Dialog.Title>{title}</Dialog.Title>
-          </Dialog.Header>
-          <Dialog.Body>
-            {content}
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Button onClick={onAction}>{actionText}</Button>
-          </Dialog.Footer>
-          <Dialog.CloseTrigger asChild>
-            <CloseButton size="sm" />
-          </Dialog.CloseTrigger>
-        </Dialog.Content>
-      </Dialog.Positioner>
-    </Portal>
-  </Dialog.Root>
-);
+const DialogComponent = ({ isOpen, onClose }) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={(e) => onClose(!e.open)}>
+      <DialogBackdrop />
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Dialog Title</DialogTitle>
+        </DialogHeader>
+        <DialogCloseTrigger />
+        
+        <DialogBody>
+          Dialog에 보일 내용/Dialog에 보일 내용/Dialog에 보일 내용/Dialog에 보일 내용/Dialog에 보일 내용/여러 줄 나오게/
+        </DialogBody>
+        
+        <DialogFooter>
+          <Button variant="outline" onClick={onClose}>
+            취소 
+          </Button>
+          <Button colorScheme="blue">
+            확인
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default DialogComponent;
