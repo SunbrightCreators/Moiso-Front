43e5f10f32876df6f4300e5ba450311cf363ac0f
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-13 11:48:51 +0900

    🎨 코드 포맷팅 (#15)

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -22,6 +22,7 @@ const globalCss = {
   'body': {
     '@supports (height: 100svh)': { height: '100svh' },
     '@supports not (height: 100svh)': { height: '100vh' },
+
     '@media (max-width: 480px)': {
       '@supports (width: 100svw)': { width: '100svw' },
       '@supports not (width: 100svw)': { width: '100vw' },
