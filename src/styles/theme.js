fd5ee35e3dbae270f267e48b4d58c1b8c92b9fb1
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-13 11:41:41 +0900

    📱 breakpoint 수정 (#15)

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -22,12 +22,12 @@ const globalCss = {
   'body': {
     '@supports (height: 100svh)': { height: '100svh' },
     '@supports not (height: 100svh)': { height: '100vh' },
-    '@media (max-width: 360px)': {
+    '@media (max-width: 480px)': {
       '@supports (width: 100svw)': { width: '100svw' },
       '@supports not (width: 100svw)': { width: '100vw' },
     },
-    '@media (min-width: 360px)': {
-      width: '360px',
+    '@media (min-width: 480px)': {
+      width: '480px',
       margin: '0 auto',
     },
   },
