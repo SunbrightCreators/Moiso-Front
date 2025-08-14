d2e2171b1d0593d3ee1057ec938878c85551793c
Min-hye-Kim <kimilu@ewha.ac.kr>
2025-08-14 21:15:29 +0900

    🎨 Dialog 컴포넌트 이름 변경 (#7)

--- src/components/common/DialogComponent.jsx
+++ src/components/common/DialogComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { Button, Dialog, Portal, CloseButton } from '@chakra-ui/react';
 import styled from 'styled-components';
 import useDialogStore from '../../stores/useDialogStore';
@@ -8,6 +9,12 @@ const SContent = styled.div`
 `;
 
 const DialogComponent = () => {
+=======
+import { Button, Dialog as ChakraDialog, Portal, CloseButton } from '@chakra-ui/react';
+import useDialogStore from '../../stores/useDialogStore';
+
+const Dialog = () => {
+>>>>>>> Stashed changes
   const {
     isOpen,
     title,
@@ -16,7 +23,11 @@ const DialogComponent = () => {
     showCancelButton,
     onAction,
     onCancel,
+<<<<<<< Updated upstream
     closeDialog,
+=======
+    closeDialog
+>>>>>>> Stashed changes
   } = useDialogStore();
 
   const handleClose = () => {
@@ -39,6 +50,7 @@ const DialogComponent = () => {
   };
 
   return (
+<<<<<<< Updated upstream
     <Dialog.Root open={isOpen} onOpenChange={(e) => !e.open && handleClose()}>
       <Portal>
         <Dialog.Backdrop />
@@ -51,6 +63,20 @@ const DialogComponent = () => {
               <SContent>{content}</SContent>
             </Dialog.Body>
             <Dialog.Footer gap={2} justifyContent="flex-end">
+=======
+    <ChakraDialog.Root open={isOpen} onOpenChange={(e) => !e.open && handleClose()}>
+      <Portal>
+        <ChakraDialog.Backdrop />
+        <ChakraDialog.Positioner>
+          <ChakraDialog.Content maxWidth="400px">
+            <ChakraDialog.Header>
+              <ChakraDialog.Title>{title}</ChakraDialog.Title>
+            </ChakraDialog.Header>
+            <ChakraDialog.Body>
+              <div style={{ whiteSpace: 'pre-line' }}>{content}</div>
+            </ChakraDialog.Body>
+            <ChakraDialog.Footer gap={2} justifyContent="flex-end">
+>>>>>>> Stashed changes
               {showCancelButton && (
                 <Button onClick={handleCancel} variant="outline">
                   취소
@@ -59,6 +85,7 @@ const DialogComponent = () => {
               <Button onClick={handleAction} colorScheme="blue">
                 {actionText}
               </Button>
+<<<<<<< Updated upstream
             </Dialog.Footer>
             <Dialog.CloseTrigger asChild>
               <CloseButton size="sm" />
@@ -67,7 +94,17 @@ const DialogComponent = () => {
         </Dialog.Positioner>
       </Portal>
     </Dialog.Root>
+=======
+            </ChakraDialog.Footer>
+            <ChakraDialog.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </ChakraDialog.CloseTrigger>
+          </ChakraDialog.Content>
+        </ChakraDialog.Positioner>
+      </Portal>
+    </ChakraDialog.Root>
+>>>>>>> Stashed changes
   );
 };
 
-export default DialogComponent;
+export default Dialog;
