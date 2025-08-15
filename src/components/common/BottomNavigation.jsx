501ad5ebebbce9124effac7c10ac97198a72b573
lloviazuu <sungjoo3321@gmail.com>
2025-08-16 02:03:58 +0900

    :lipstick: BottomNavigation 이미지 (#21)

---
+++ src/components/common/BottomNavigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import useModeStore from '../../stores/useModeStore';
+
+import { ReactComponent as ProposalIcon } from '../../assets/icons/BNB_proposal.svg';
+import { ReactComponent as FundingIcon } from '../../assets/icons/BNB_funding.svg';
+import { ReactComponent as RewardIcon } from '../../assets/icons/BNB_reward.svg';
+import { ReactComponent as MyIcon } from '../../assets/icons/BNB_my.svg';
+import { ReactComponent as RecommendIcon } from '../../assets/icons/BNB_rec.svg';
--- src/stores/useModeStore.js
+++ src/stores/useModeStore.js
@@ -3,7 +3,6 @@ import { create } from 'zustand';
 const MODE_KEY = 'userMode'; // 로컬스토리지 키 이름
 
 const useUserModeStore = create((set) => ({
-  // 초기값: localStorage에서 읽어오기 (없으면 null)
   userMode: localStorage.getItem(MODE_KEY) || null,
 
   // 상태 변경 함수 + localStorage 동기화
