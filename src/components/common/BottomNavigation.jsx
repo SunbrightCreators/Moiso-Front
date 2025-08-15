56008f502cfc6f32143ef92aeac6cf46991608d6
lloviazuu <sungjoo3321@gmail.com>
2025-08-16 02:04:44 +0900

    :lipstick: 창업자와 지역주민일때 구분

--- src/components/common/BottomNavigation.jsx
+++ src/components/common/BottomNavigation.jsx
@@ -7,3 +7,17 @@ import { ReactComponent as FundingIcon } from '../../assets/icons/BNB_funding.sv
 import { ReactComponent as RewardIcon } from '../../assets/icons/BNB_reward.svg';
 import { ReactComponent as MyIcon } from '../../assets/icons/BNB_my.svg';
 import { ReactComponent as RecommendIcon } from '../../assets/icons/BNB_rec.svg';
+
+const residentNavItems = [
+  { path: '/proposal', label: '제안', Icon: ProposalIcon },
+  { path: '/funding', label: '펀딩', Icon: FundingIcon },
+  { path: '/reward', label: '리워드', Icon: RewardIcon },
+  { path: '/my', label: '마이', Icon: MyIcon },
+];
+
+const founderNavItems = [
+  { path: '/proposal', label: '제안', Icon: ProposalIcon },
+  { path: '/recommend', label: '추천', Icon: RecommendIcon },
+  { path: '/funding', label: '펀딩', Icon: FundingIcon },
+  { path: '/my', label: '마이', Icon: MyIcon },
+];
