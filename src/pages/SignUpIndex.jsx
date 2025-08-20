74c01a599034e8aeaf5dc9f4be9178b6521ccec0
lloviazuu <sungjoo3321@gmail.com>
2025-08-20 18:45:19 +0900

    🎨 SignupIndex구조 변경

--- src/pages/SignUpIndex.jsx
+++ src/pages/SignUpIndex.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import useModeStore from '../stores/useModeStore';
 
 import SignUpPage1 from './SignUpPage1';
 import SignUpPage2 from './SignUpPage2';
@@ -11,15 +10,7 @@ const stepComponents = [
 
 function SignUpIndex() {
   const [currentStep, setCurrentStep] = useState(0);
-  const setIsProposerMode = useModeStore((state) => state.setIsProposerMode);
-
-  const goToNextStep = (isProposer) => {
-    // isProposer가 undefined가 아닌 경우에만 setIsProposerMode 호출
-    if (typeof isProposer === 'boolean') {
-      setIsProposerMode(isProposer);
-    }
-    setCurrentStep((prevStep) => prevStep + 1);
-  };
+  const goToNextStep = () => setCurrentStep((prev) => prev + 1);
 
   const renderStep = () => {
     // 배열에서 현재 단계에 해당하는 컴포넌트를 가져옵니다.
--- src/pages/SignUpPage1.jsx
+++ src/pages/SignUpPage1.jsx
@@ -43,43 +43,51 @@ const LogoBox = styled(Box)`
 function SignUpPage1({ onNextStep }) {
   // '창업자로 가입' 버튼 클릭 시 실행될 함수
   const handleFounderClick = () => {
-    if (onNextStep && typeof onNextStep === 'function') {
-      onNextStep(false);
-    } else {
-      console.error('onNextStep is not a valid function.');
-    }
+    setIsProposerMode(false); // 창업자 모드
+    onNextStep?.();
   };
 
   // '지역주민으로 가입' 버튼 클릭 시 실행될 함수
   const handleResidentClick = () => {
-    if (onNextStep && typeof onNextStep === 'function') {
-      onNextStep(true);
-    } else {
-      console.error('onNextStep is not a valid function.');
-    }
-  };
-  return (
-    <Flex direction='column' minH='100%' font='font: var(--text-sm-semibold);'>
-      <VStack spacing={4} flex='1' justify='center'>
-        <LogoBox>
-          <img src={logo} alt='서비스 로고' width='150' height='150' />
-        </LogoBox>
-        <VStack spacing={2}>
-          <Heading as='h1'>서비스 슬로건</Heading>
-          <Text>주민과 창업자를 이어주는 크라우드펀딩 플랫폼</Text>
-        </VStack>
-      </VStack>
+    const handleResidentClick = () => {
+      setIsProposerMode(true); // 주민 모드
+      onNextStep?.();
+    };
 
-      <VStack spacing={3} w='100%' maxW='md' mx='auto' mt='1.44rem' pb='5.5rem'>
-        <StyledLoginButton onClick={handleFounderClick}>
-          창업자로 가입
-        </StyledLoginButton>
-        <StyledLoginButton onClick={handleResidentClick}>
-          지역주민으로 가입
-        </StyledLoginButton>
-      </VStack>
-    </Flex>
-  );
+    return (
+      <Flex
+        direction='column'
+        minH='100%'
+        font='font: var(--text-sm-semibold);'
+      >
+        <VStack spacing={4} flex='1' justify='center'>
+          <LogoBox>
+            <img src={logo} alt='서비스 로고' width='150' height='150' />
+          </LogoBox>
+          <VStack spacing={2}>
+            <Heading as='h1'>서비스 슬로건</Heading>
+            <Text>주민과 창업자를 이어주는 크라우드펀딩 플랫폼</Text>
+          </VStack>
+        </VStack>
+
+        <VStack
+          spacing={3}
+          w='100%'
+          maxW='md'
+          mx='auto'
+          mt='1.44rem'
+          pb='5.5rem'
+        >
+          <StyledLoginButton onClick={handleFounderClick}>
+            창업자로 가입
+          </StyledLoginButton>
+          <StyledLoginButton onClick={handleResidentClick}>
+            지역주민으로 가입
+          </StyledLoginButton>
+        </VStack>
+      </Flex>
+    );
+  };
 }
 
 export default SignUpPage1;
