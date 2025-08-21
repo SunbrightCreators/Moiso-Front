680b86aa9a04019b02e8fd2c2dafccd21f4739b0
lloviazuu <sungjoo3321@gmail.com>
2025-08-21 14:34:41 +0900

    :router.jsx 복구

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,22 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LogInPage';
-import SignUpIndex from './pages/SignUpIndex';
-import NeighborhoodSettingsPage from './pages/NeighborhoodSettingsPage';
-import FounderTarget from './pages/FounderTarget';
-import FounderTime from './pages/FounderTime';
-import WelcomePage from './pages/WelcomePage';
-
-import { ROUTE_PATH } from './constants/route';
-import OnBaoardingSelect from './pages/OnBardingSelect';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path={ROUTE_PATH.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTE_PATH.SIGNUP} element={<SignUpIndex />} />
+        <Route path='/' element={<div>홈</div>} />
       </Routes>
     </BrowserRouter>
   );
