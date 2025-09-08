7b9d5923055cf90bdf2712e39da2bef77be3373d
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-09-08 23:11:02 +0900

    🚚 TopNavigation, BottomNavigation 컴포넌트 components/common/navigation 폴더로 이동

--- src/components/common/navigation/BottomNavigation.jsx
+++ src/components/common/navigation/BottomNavigation.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import useModeStore from '../../stores/useModeStore';
+import useModeStore from '../../../stores/useModeStore';
 import styled from 'styled-components';
-import { ROUTE_PATH } from '../../constants/route';
+import { ROUTE_PATH } from '../../../constants/route';
 
 import { ReactComponent as ProposalIcon } from '../../assets/icons/BNB_proposal.svg';
 import { ReactComponent as FundingIcon } from '../../assets/icons/BNB_funding.svg';
